814c9a038074be0c77f5fec90880be626d9fb07b
mahbubmanna <mahbubmanna@yahoo.com>
2021-08-20 03:35:59 +0600

    deleted all unnecessary comment

--- js/banking.js
+++ js/banking.js
@@ -32,9 +32,6 @@ function getCurrentBalance() {
 function updateBalance(amount, isAdd) {
     const balanceTotal = document.getElementById('balance-total');
 
-    /*  const balanceTotalText = balanceTotal.innerText;
-     const prvBalanceTotal = parseFloat(balanceTotalText); */
-
     const prvBalanceTotal = getCurrentBalance();
 
     if (isAdd == true) {
@@ -48,60 +45,16 @@ function updateBalance(amount, isAdd) {
 // Handling Deposit button
 
 document.getElementById('deposit-button').addEventListener('click', function () {
-    // getting the deposit amount
-    /* const depositInput = document.getElementById('deposit-input');
-
-    const depositAmountText = depositInput.value;
-    const depositAmount = parseFloat(depositAmountText); */
-
-    // updating current deposit
-    /* const depositTotal = document.getElementById('deposit-total');
-
-    const depositTotalText = depositTotal.innerText;
-    const prvDepositTotal = parseFloat(depositTotalText);
-
-    depositTotal.innerText = prvDepositTotal + depositAmount; */
-
-    // updating account balance
-    /* const balanceTotal = document.getElementById('balance-total');
-
-    const balanceTotalText = balanceTotal.innerText;
-    const prvBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerText = prvBalanceTotal + depositAmount; */
 
     const depositAmount = getInputValue('deposit-input');
     if (depositAmount > 0) {
         updateTotalField('deposit-total', depositAmount);
         updateBalance(depositAmount, true);
     }
-
-    /* // clear the deposit input-field
-    depositInput.value = ''; */
 })
 
 // Handling Withdraw Button
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    /* const withdrawInput = document.getElementById('withdraw-input');
-
-    const withdrawAmountText = withdrawInput.value;
-    const withdrawAmount = parseFloat(withdrawAmountText); */
-
-    // updating withdraw total
-    /* const withdrawTotal = document.getElementById('withdraw-total');
-
-    const withdrawTotalText = withdrawTotal.innerText;
-    const prvWindrawTotal = parseFloat(withdrawTotalText);
-
-    withdrawTotal.innerText = prvWindrawTotal + withdrawAmount; */
-
-    //updating balance after withdraw
-    /* const balanceTotal = document.getElementById('balance-total');
-
-    const balanceTotalText = balanceTotal.innerText;
-    const prvBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerText = prvBalanceTotal - withdrawAmount; */
 
     const withdrawAmount = getInputValue('withdraw-input');
     const currentBalance = getCurrentBalance();
